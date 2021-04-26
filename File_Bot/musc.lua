29282f229a746f4cbd1f0be1868245db08442cdb
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-04-26 22:10:40 +0200

    Add files via upload

--- File_Bot/musc.lua
+++ File_Bot/musc.lua
@@ -1,6 +1,6 @@
 local function Reply(msg)
 local text = msg.content_.text_
-local wegz1 = 'curl "'..'https://api.telegram.org/bot'.. token ..'/sendDocument'..'" -F "chat_id='.. msg.chat_id_ ..'" -F "document=@'..'DRAGON.lua'..'"'
+local wegz1 = 'curl "'..'https://api.telegram.org/bot'.. token ..'/sendDocument'..'" -F "chat_id='.. msg.chat_id_ ..'" -F "document=@'..'Wegz - El Ghasala (El Ghasala Official Movie Song) - (ويجز - الغسالة (الأغنية الرسمية لفيلم الغسالة(MP3_160K)_1.mp3'..'"'
 
 if text == 'ويجز 1' then
 io.popen(wegz1)
